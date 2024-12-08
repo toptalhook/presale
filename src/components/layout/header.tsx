09fe17a06234b2b3c3c726b7ed4a0cd4d1d236bd
toptalhook <toptal.cook@gmail.com>
2024-12-08 11:41:06 +0900

    init

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,17 @@
+
+function Header() {
+    return (
+        <header>
+            <div className="header-content">
+                <div className="logo">
+                    <img src="/assets/logo.svg" alt="DAO Maker Logo" />
+                </div>
+                {/* <appkit-button>
+                    Connect Wallet
+                </appkit-button> */}
+            </div>
+        </header>
+    )
+}
+
+export default Header
